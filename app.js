5d42dfd4aa8702ee4ee25e184dac509ce5c7d6d0
Jay Pillay <jpillay@twilio.com>
2020-03-29 19:38:10 -0400

    added express app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var twilio = require('twilio');
 
 // Load configuration information from system environment variables.
-var TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
+var TWILIO_ACCOUNT_SID = process.env.   TWILIO_ACCOUNT_SID,
     TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
     TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
 
